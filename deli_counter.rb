2548b3d650983fbbb787db17014511c007ca1bf5
Stephen Wright <scw248@gmail.com>
2019-01-04 15:43:05 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+
+def line(array)
+  if array.length == 0
+    puts "The line is currently empty."
+  else
+    customer_line = "The line is currently:"
+    array.each do |customer|
+    customer_line += " #{customer.index}. #{customer}"
+  end
+  puts customer_line
+end
+end
+
+def take_a_number(array, customer)
+  if array.length == 0
+    puts "Welcome, #{customer} You are number 1 in line."
+    array.push(customer)
+  else
+    
+    puts "Welcome, #{customer}. You are number  in line."
+  end
+end
+
